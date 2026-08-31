367a734ea2b2e6ae4cba022c6805c7c7f49f568f
bgravley <bgravley@rdmarketingllc.com>
2026-08-31 08:50:48 -0500

    Mount purchase confirmation analytics bridge

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import PurchaseAnalyticsBridge from './PurchaseAnalyticsBridge'
 import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <PurchaseAnalyticsBridge />
     <Analytics />
   </React.StrictMode>
 )
